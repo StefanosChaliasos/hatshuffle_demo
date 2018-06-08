54e39a256058370f36dc38b315bb2444c35342a7
StefanosChaliasos <t8140192@dias.aueb.gr>
2018-06-08 17:26:09 +0300

    Add decryption (client side) and benchmark page

--- web/benchmark.js
+++ web/benchmark.js
@@ -6,18 +6,17 @@ let ciphertexts = [];
 let shuffled_ciphertexts;
 let table;
 let decrypted_votes;
+let ballots_number = 0;
+let voters_number = 0;
+
 
 function decrypt() {
     let vector_ciphers = new Module.Ciphertexts();
     for (var property1 in shuffled_ciphertexts) {
         for (var property2 in shuffled_ciphertexts[property1]) {
-            // console.log(shuffled_ciphertexts[property1][property2][0]);
             vector_ciphers.push_back(shuffled_ciphertexts[property1][property2][0]);
-            // console.log(shuffled_ciphertexts[property1][property2][1]);
             vector_ciphers.push_back(shuffled_ciphertexts[property1][property2][1]);
-            // console.log(shuffled_ciphertexts[property1][property2][2]);
             vector_ciphers.push_back(shuffled_ciphertexts[property1][property2][2]);
-            // console.log(shuffled_ciphertexts[property1][property2][3]);
             vector_ciphers.push_back(shuffled_ciphertexts[property1][property2][3]);
         }
     }
@@ -25,49 +24,37 @@ function decrypt() {
     return Module.decrypt(table, vector_ciphers, sk_neg);
 }
 
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
 $(document).ready(function() {
+    $('#set').click(function() {
+        voters_number = + $('#voters').val();
+        ballots_number =  +  $('#ballots').val();
+    });
     $('#keygen').click(function() {
+        t0 = performance.now();
         keys = Module.keygen();
+        t1 = performance.now();
         sk = keys.split(",")[0];
         pk = keys.split(",")[1];
-        $("#sk").text(sk);
-        $("#pk").text(pk);
+        $("#keys").text("" + (t1 - t0) + " miliseconds" );
         publickey = [pk.split(' ')[1], pk.split(' ')[2], pk.split(' ')[3],
               pk.split(' ')[4]];
     });
-    $('#vote').click(function() {
-        choice = $('select[name=mvp]').val();
-        cipher = Module.vote(pk, choice);
-        $("#cipher").text(cipher);
-        cipher = cipher.split(',');
-        c1 = [cipher[0].split(' ')[1], cipher[0].split(' ')[2],
-              cipher[0].split(' ')[3], cipher[0].split(' ')[4]];
-        c2 = [cipher[1].split(' ')[1], cipher[1].split(' ')[2],
-              cipher[1].split(' ')[3], cipher[1].split(' ')[4]];
-        ciphertext = [c1, c2];
-        ciphertexts.push(ciphertext);
-        votes_counter++;
-    });
     $('#mixnet').click(function() {
+        t0 = performance.now();
+        // voting
+        for (i = 0; i < voters_number; i++) {
+            choice = "" + (Math.floor(Math.random() * ballots_number));
+            cipher = Module.vote(pk, choice);
+            cipher = cipher.split(',');
+            c1 = [cipher[0].split(' ')[1], cipher[0].split(' ')[2],
+                  cipher[0].split(' ')[3], cipher[0].split(' ')[4]];
+            c2 = [cipher[1].split(' ')[1], cipher[1].split(' ')[2],
+                  cipher[1].split(' ')[3], cipher[1].split(' ')[4]];
+            ciphertext = [c1, c2];
+            ciphertexts.push(ciphertext);
+            votes_counter++;
+        }
+        // mixnet
         serialized_ciphers = {};
         serialized_pk = {};
         serialized = {};
@@ -86,15 +73,22 @@ $(document).ready(function() {
             async       : true,
             encode      : true
         }).done(function(data) {
-            $("#ver").text(data["verify"]);
             json_ciphers = data;
             shuffled_ciphertexts = json_ciphers["output"];
             delete json_ciphers["verify"];
             str_ciphers = JSON.stringify(json_ciphers, undefined, 4);
-            $("#shuffled").html(syntaxHighlight(str_ciphers));
         });
+        t1 = performance.now();
+        $("#mix-verify").text("" + (t1 - t0) + " miliseconds" );
+    });
+    $('#create_table').click(function() {
+        t0 = performance.now();
+        table = Module.create_table(3);
+        t1 = performance.now();
+        $("#table").text("" + (t1 - t0) + " miliseconds");
     });
     $('#decrypt_server').click(function() {
+        t0 = performance.now();
         serialized_secret = {};
         serialized_secret["secret"] = sk;
         $.ajax({
@@ -108,19 +102,18 @@ $(document).ready(function() {
         }).done(function(data) {
             json_votes = data;
             str_votes = JSON.stringify(json_votes, undefined, 4);
-            $("#decrypted_server").html(syntaxHighlight(str_votes));
+            t1 = performance.now();
+            $("#decrypted_server").text("" + (t1 - t0) + " miliseconds");
         });
     });
-    $('#create_table').click(function() {
-        table = Module.create_table(3);
-        $("#table").text(table.size());
-    });
     $('#decrypt_client').click(function() {
+        t0 = performance.now();
         decrypted_votes = decrypt();
         d_votes = "";
         for (i = 0; i < decrypted_votes.size(); i++) {
             d_votes += "" + decrypted_votes.get(i) + " ";
         }
-        $("#decrypted_client").html(d_votes);
+        t1 = performance.now();
+        $("#decrypted_client").html("" + (t1 - t0) + " miliseconds");
     });
 });
